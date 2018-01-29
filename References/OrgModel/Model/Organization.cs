6a3975c487e5f60745643371707f0135acc590ee
Dimon <meldimo@gmail.com>
2018-01-29 18:39:36 +0200

    from work

--- References/OrgModel/Model/Organization.cs
+++ References/OrgModel/Model/Organization.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace com.sbh.gui.references.orgmodel.Model
 {
-    public class Organization
+    public class Organization 
     {
         public int id { get; set; }
         public string name { get; set; }
         public int refStatus { get; set; }
         public ObservableCollection<Branch> Branches { get; set; }
+
     }
 }
---
+++ References/OrgModel/ViewModel/UCOrgModelViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.sbh.gui.references.orgmodel.ViewModel
+{
+    public class UCOrgModelViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Model.Organization> _organization { get; set; }
+
+        public ObservableCollection<Model.Organization> Organization 
+        {
+            get { return _organization; }
+            set
+            {
+                _organization = value;
+                OnPropertyChanged("Organization");
+            }
+        }
+
+        public void collectOrganization()
+        {
+
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
