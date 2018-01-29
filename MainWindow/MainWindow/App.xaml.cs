e4a9a1c45dbe775cbf0bd9c2e4f967a01bd936a0
Dimon <meldimo@gmail.com>
2018-01-29 14:23:16 +0200

    from work

--- MainWindow/MainWindow/App.xaml.cs
+++ MainWindow/MainWindow/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace MainWindow
+namespace com.sbh.gui.mainwindow
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- MainWindow/MainWindow/MainWindow.xaml.cs
+++ MainWindow/MainWindow/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MainWindow
+namespace com.sbh.gui.mainwindow
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
