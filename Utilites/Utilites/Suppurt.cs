2acbf8bd16e944f82eaceba963778d28d1a1ad82
Dimon <meldimo@gmail.com>
2018-01-30 11:33:07 +0200

    from work

---
+++ Utilites/Utilites/Suppurt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace com.sbs.dll.utilites
+{
+    public class Suppurt
+    {
+        public static T XMLToObject<T>(string xml)
+        {
+
+            var serializer = new XmlSerializer(typeof(T));
+
+            using (var textReader = new StringReader(xml))
+            {
+                using (var xmlReader = XmlReader.Create(textReader))
+                {
+                    return (T)serializer.Deserialize(xmlReader);
+                }
+            }
+        }
+    }
+}
