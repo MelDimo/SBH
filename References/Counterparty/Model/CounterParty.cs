7dc0a1300ff64ba7ce8aa0968a2382bc71e80928
Dimon <meldimo@gmail.com>
2018-02-07 19:18:44 +0200

    from work

---
+++ References/Counterparty/Model/CounterParty.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.sbh.gui.references.counterparty.Model
+{
+    public class CounterParty : INotifyPropertyChanged
+    {
+        public decimal id { get; set; }
+
+        private string _name;
+        public string name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged("name");
+            }
+        }
+
+        private string _description;
+        public string description
+        {
+            get { return _description; }
+            set
+            {
+                _description = value;
+                OnPropertyChanged("description");
+            }
+        }
+
+        private string _groupname;
+        public string groupname
+        {
+            get { return _groupname; }
+            set
+            {
+                _groupname = value;
+                OnPropertyChanged("groupname");
+            }
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+
+        #endregion
+    }
+}
