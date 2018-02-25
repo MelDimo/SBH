47092ae483a899f7a43d69112f9a48c2d2fef203
Dimon <meldimo@gmail.com>
2018-02-25 10:30:02 +0200

    from work

---
+++ References/Item/ViewModel/SurfaceItemViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.sbh.gui.references.item.ViewModel
+{
+    public class SurfaceItemViewModel : INotifyPropertyChanged
+    {
+
+        public SurfaceItemViewModel()
+        {
+
+        }
+
+
+
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
---
+++ Utilites/Utilites/OReferences/RefItem.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace com.sbh.dll.utilites.OReferences
+{
+    public sealed class RefItem
+    {
+        private RefItem()
+        {
+            reload();
+        }
+
+        public void reload()
+        {
+            using (SqlConnection con = new SqlConnection(GValues.connString))
+            {
+                con.Open();
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = con;
+                    command.CommandText = " SELECT id, name " +
+                                            " FROM item " +
+                                            " FOR XML RAW('Item'), ROOT('ArrayOfItem'), ELEMENTS ";
+
+                    XmlReader reader = command.ExecuteXmlReader();
+                    while (reader.Read())
+                    {
+                        refItem = Support.XMLToObject<List<Item>>(reader.ReadOuterXml());
+                    }
+                }
+            }
+        }
+
+        private static readonly Lazy<RefItem> lazy = new Lazy<RefItem>(() => new RefItem());
+
+        public static RefItem GetInstance { get { return lazy.Value; } }
+
+        private List<Item> _refItem;
+        public List<Item> refItem
+        {
+            get { return _refItem; }
+            private set { _refItem = value; }
+        }
+
+        public class Item
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+        }
+    }
+}
