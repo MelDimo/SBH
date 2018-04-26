71db3140f09d8aae3d265cbcdfc7a5f1c90cc413
Dimon <meldimo@gmail.com>
2018-04-26 18:48:34 +0300

    from work

---
+++ UnitsBrowser/UnitsBrowser/ViewModel/BaseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitsBrowser.ViewModel
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public BaseViewModel ViewModel { get; set; }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
