a851b6ce29cdddd55a6d528d2b9320d99d42d8e5
Dimon <meldimo@gmail.com>
2018-06-21 09:44:46 +0300

    from work

---
+++ CoreSolution/Core/Middleware/ErrorHandingMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Middleware
+{
+    public class ErrorHandingMiddleware
+    {
+        RequestDelegate next;
+
+        public ErrorHandingMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, DTO.MSG ms)
+        {
+            await next(context);
+
+            switch (ms.Code)
+            {
+                case (int)GValues.GValues.MSGCodes.ERROR:
+                    break;
+            }
+        }
+    }
+}
---
+++ CoreSolution/GValues/MSGCodes.cs
@@ -0,0 +1,11 @@
+﻿namespace GValues
+{
+    public partial class GValues
+    {
+        public enum MSGCodes
+        {
+            OK = 10001,
+            ERROR = 10002
+        }
+    }
+}
